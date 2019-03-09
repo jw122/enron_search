9651a01064fd136c19342db444dae8485f086652
jw122 <thejuliawu@gmail.com>
2019-03-09 12:29:53 -0800

    comments in process.py

--- process.py
+++ process.py
@@ -7,9 +7,10 @@ maildir = "test"
 
 # TODO: pass in file as argument
 
+# A word map from word -> {author: [list of emails]}
 words_map = {}
 for filename in os.listdir(maildir):
-
+    # Loop through all the individuals in the maildir
     person = filename
     person_dir = maildir + "/" + person
     print("reading contents in this person's mailbox: " + person)
@@ -18,15 +19,18 @@ for filename in os.listdir(maildir):
         continue
     else:
         all_docs = person_dir + "/" + "all_documents"
-
+        # Open the files in this person's "all_documents" folder
         for file in os.listdir(all_docs):
             filePath = all_docs + "/" + file
             with open(filePath) as f:
                 contents = f.readlines()
                 for line in contents:
                     words = line.split()
+                    # TODO: can scan for delimiter to avoid words in email metadata
                     for w in words:
+                        # Get rid of non alphanumeric characters
                         word = re.sub(r'\W+', '', w)
+                        # Add the word and its author + email id to the map
                         if len(word) > 0:
                             if word not in words_map:
                                 words_map[word] = {person: [file]}
